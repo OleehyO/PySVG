56d411e07cc65d9bffbc2753bba5b62d50f5938d
lihaoyang <leehy0357@gmail.com>
2025-06-24 05:04:21 +0000

    [feat]: Add utility modules for iteration and matrix operations
    Add new utility modules to enhance SVG manipulation capabilities:
    - iterator.py: Implement iteration utilities
    - matrix.py: Add matrix operation helpers
    - nest_list.py: Add nested list manipulation utilities
    - Update __init__.py and misc.py to integrate new modules

--- pysvg/utils/__init__.py
+++ pysvg/utils/__init__.py
@@ -1,5 +1,7 @@
 from .path import resolve_path, touch, mkdir, rmfile, rmdir
-from .misc import change_nestls_type, get_nestls_elem
+from .nest_list import change_nestls_type, get_nestls_elem
+from .iterator import MatrixIterator, MatrixBoardIterator
+from .matrix import add_matrix_border
 
 __all__ = [
     "resolve_path",
@@ -9,4 +11,7 @@ __all__ = [
     "rmdir",
     "change_nestls_type",
     "get_nestls_elem",
+    "MatrixIterator",
+    "MatrixBoardIterator",
+    "add_matrix_border",
 ]
